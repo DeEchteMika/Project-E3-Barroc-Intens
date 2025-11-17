828d15ddaa0ba487988eb151345f5acb58b068f7
DeEchteMika <D299024@edu.curio.nl>
2025-11-17 10:31:04 +0100

    added sentence to dashboard management
    and changed pagination in app layout

--- resources/views/dashboards/managementDashboard.blade.php
+++ resources/views/dashboards/managementDashboard.blade.php
@@ -1,4 +1,4 @@
 <x-app-layout>
-
+<h1>deze pagina werkt</h1>
 
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
+
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
@@ -33,4 +35,6 @@
             </main>
         </div>
     </body>
+
+
 </html>
