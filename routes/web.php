d819b6ac467db8184058a775efbbce7ee436d7d4
monkeshrek <d303802@edu.curio.nl>
2025-11-24 10:09:49 +0100

    web routes fix

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,35 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InkoopController;
+use App\Http\Controllers\InkoopRegelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::get('/Management-Dashboard', function () {
+    return view('dashboards.managementDashboard');
+})->name('managementDashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/sales-dashboard', function () {
+    return view('sales');
+})->middleware(['auth', 'verified'])->name('sales.dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Simple product management for inkoop: list, create, store, destroy
+    Route::get('inkoop', [InkoopController::class, 'index'])->name('inkoop.index');
+    Route::get('inkoop/create', [InkoopController::class, 'create'])->name('inkoop.create');
+    Route::post('inkoop', [InkoopController::class, 'store'])->name('inkoop.store');
+    Route::delete('inkoop/{product}', [InkoopController::class, 'destroy'])->name('inkoop.destroy');
 });
 
 require __DIR__.'/auth.php';
