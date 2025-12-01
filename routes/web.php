f4e4b45ba6d0c4190acf34dbcba4de25372b4087
ZeroTwo <hoes.romeo@gmail.com>
2025-12-01 11:57:40 +0100

    added change web

--- routes/web.php
+++ routes/web.php
@@ -12,48 +12,38 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-// Dashboard
 Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-// Auth + verified: sales & customers
 Route::middleware(['auth', 'verified'])->group(function () {
-    // Sales dashboard
     Route::get('/sales-dashboard', function () {
         return view('sales.sales-dashboard');
     })->name('sales.dashboard');
 
-    // Customers (index, create, store)
     Route::resource('customers', CustomerController::class)
         ->only(['index', 'create', 'store']);
 
-    // Alias route for the same create form (Sales form button)
     Route::get('/sales-create', [CustomerController::class, 'create'])
         ->name('sales.create');
 });
 
-// Auth only: profile, inkoop, financien
 Route::middleware('auth')->group(function () {
-    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Simple product management for inkoop: list, create, store, destroy
     Route::get('inkoop', [InkoopController::class, 'index'])->name('inkoop.index');
     Route::get('inkoop/create', [InkoopController::class, 'create'])->name('inkoop.create');
     Route::post('inkoop', [InkoopController::class, 'store'])->name('inkoop.store');
     Route::delete('inkoop/{product}', [InkoopController::class, 'destroy'])->name('inkoop.destroy');
 
-    // Financien routes
     Route::get('financien', [FinancienController::class, 'index'])->name('financien.index');
     Route::get('financien/{klant}/edit', [FinancienController::class, 'edit'])->name('financien.edit');
     Route::get('financien/create', [FinancienController::class, 'create'])->name('financien.create');
     Route::post('financien', [FinancienController::class, 'store'])->name('financien.store');
     Route::put('financien/update', [FinancienController::class, 'update'])->name('financien.update');
 
-    // Test mail routes
     Route::get('/send-mail-test', [ProcessMailController::class, 'showForm'])->name('send.mail.form');
     Route::post('/send-mail-test', [ProcessMailController::class, 'send'])->name('send.mail.send');
 });
@@ -71,7 +61,6 @@ Route::get('/sales', function () {
     return view('sales.index');
 })->name('sales');
 
-// Public / no-auth pages (if that's intended)
 Route::get('/klantenservice', function () {
     return view('klantenservice.index');
 })->name('klantenservice');
